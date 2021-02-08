5cab56015d0928d089fd1e3b1624c64d4206f91a
chonnavich <chonnavich.rod@dome.tu.ac.th>
2021-02-08 16:30:43 +0700

    555

---
+++ main.js
@@ -0,0 +1,13 @@
+function sum(a, b) {
+    if (Array.isArray(a)) {
+        return a.reduce(total,i) => total + i,0)
+      } 
+        return a + b
+      }
+    
+  
+
+  module.exports = {
+    sum,
+  }
+  
